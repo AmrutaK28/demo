0c99340a1e77253b5cdf41775641106b11dfdd05
Amruta <amrutaukangiri@gmail.com>
2021-03-27 11:15:38 +0530

    Initialize commit

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -17,6 +17,8 @@ public class Demo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("ddfdff");
+        System.out.println("ddfdff");
+        
     }
     
 }
